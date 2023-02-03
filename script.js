a337a86d0bb8a6c57256f5856b60ac05ae63e7f2
codedit334 <oussama7elabdioui@gmail.com>
2023-02-03 09:30:44 +0100

    Add 1 bug

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const list = document.querySelector('.list');
 
-for (let i = 1; i < 3; i++) list.innerHTML += `<li>Bug: ${i}</li>`;
+for (let i = 1; i < 3; i+) list.innerHTML += `<li>Bug: ${i}</li>`;
